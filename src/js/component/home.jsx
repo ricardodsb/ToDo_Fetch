766e73296561ccaa47c30e31ac7d209f22b01daa
ricardodsb <ricardojdasilva15@gmail.com>
2021-10-25 10:57:15 +0000

    trying to add fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 useEffect(() => {
 	fetch("https://assets.breatheco.de/apis/fake/todos/user/ricardodsb", {
